125931695eedcea8683940eca5ca90b93ac178b2
Ajunmiiin <wjdals3249@naver.com>
2025-02-13 22:55:43 +0900

    feat : 기본 라우트 설정, Home, Login 페이지 추가

---
+++ src/routes/pages/Login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '../../components/auth/Login/LoginForm';
+
+const Login = () => {
+  return (
+    <main className="flex min-h-[100svh] flex-col justify-between p-5 pt-[150px]">
+      <LoginForm />
+    </main>
+  );
+};
+
+export default Login;
