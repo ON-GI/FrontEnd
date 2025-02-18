9dd14e869b9a303e61fc4854cf669cf7c8fda9dc
Ajunmiiin <wjdals3249@naver.com>
2025-02-18 23:20:33 +0900

    feat : 로그인 성공 시 토큰 저장

--- src/api/instnace.ts
+++ src/api/instnace.ts
@@ -1,10 +1,28 @@
 import axios from 'axios';
+import { getCookie } from '../utils/cookie';
+
+const accessToken = getCookie('token');
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': `application/json`,
+    Accept: '*/*',
   },
+
+  withCredentials: true,
 });
 
 export default client;
+
+client.interceptors.request.use(
+  (config) => {
+    const accessToken = getCookie('accessToken');
+    console.log(accessToken);
+    if (accessToken) {
+      config.headers!.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
--- src/components/auth/Login/useLogin.ts
+++ src/components/auth/Login/useLogin.ts
@@ -1,21 +1,27 @@
 import { useMutation } from '@tanstack/react-query';
 
 import client from '../../../api/instnace';
+import { setCookie } from '../../../utils/cookie';
 
 const useLogin = (authority: 'ROLE_CAREGIVER' | 'ROLE_CENTER') => {
   return useMutation({
     mutationFn: async ({ id, password }: { id: string; password: string }) => {
-      const response = await client.post('/auth/login', {
-        id,
-        password,
-        authority,
-      });
+      try {
+        const response = await client.post('/auth/login', {
+          loginId: id,
+          password: password,
+          authority,
+        });
 
-      if (response.status !== 200) {
-        throw new Error(response.data.message);
+        if (response.status !== 200) {
+          throw new Error(response.data.message);
+        }
+        const token = response.data.data.accessToken.split(' ')[1];
+
+        setCookie('accessToken', token);
+      } catch (error) {
+        console.log(error);
       }
-
-      // 로그인 성공 후속처리 필요
     },
   });
 };
