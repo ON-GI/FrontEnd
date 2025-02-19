40b4c4a252e3172239d1edc0236f7371e0265d26
Ajunmiiin <wjdals3249@naver.com>
2025-02-19 20:41:41 +0900

    feat : accessToken 만료시 refreshToken 갱신 코드 추가

--- src/api/instnace.ts
+++ src/api/instnace.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { getCookie } from '../utils/cookie';
+import { getCookie, removeCookie } from '../utils/cookie';
+import refreshToken from './Auth/refreshToken';
 
 const accessToken = getCookie('token');
 
@@ -18,11 +19,38 @@ export default client;
 client.interceptors.request.use(
   (config) => {
     const accessToken = getCookie('accessToken');
-    console.log(accessToken);
     if (accessToken) {
       config.headers!.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+client.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    console.log(error.response?.data);
+
+    const originalRequest = error.config;
+
+    // 401 에러가 발생하고, 재시도를 한 적이 없는 경우만 처리
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await refreshToken();
+        return client(originalRequest); // 원래 요청 재시도
+      } catch {
+        if (axios.isAxiosError(error)) {
+          throw new Error(error.response?.data.message);
+        } // refreshToken 실패 시 오류 반환
+      }
+    }
+
+    // 401이 아닌 경우 일반적으로 오류 반환
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    }
+  },
 );
